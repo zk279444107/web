61fcf9284b201d84353237910bd0da2f55ea67fd
zk <zk279444107@gmail.com>
2015-09-17 23:48:12 +0800

    121212

--- src/main/java/com/zkweb/controller/SessionController.java
+++ src/main/java/com/zkweb/controller/SessionController.java
@@ -12,13 +12,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 
-@Controller 
+@Controller
 public class SessionController {
 	@RequestMapping(value = "/test/session", method = RequestMethod.GET)
 	@ResponseBody
-	public String sessionTest(HttpServletRequest request){
-		Map<String,String> map = new HashMap<String,String>();
+	public String sessionTest(HttpServletRequest request) {
+		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("session", request.getSession().toString());
+		map.put("key1", request.getSession().getId());
+		map.put("key2", "汉字测试");
+		Map<String, Object> map1 = new HashMap<String, Object>(map);
+		map.put("key3", map1);
 		return JSON.toJSONString(map);
 	}
 }
